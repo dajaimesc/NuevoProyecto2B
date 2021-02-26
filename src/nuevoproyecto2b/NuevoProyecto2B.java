d0c37e9e75bcb2aaf76b420bc1552249e8401fbb
danieljaimes <danieljaimes@192.168.0.8>
2021-02-25 19:25:11 -0600

    Primer commit de NuevoProyecto 2B

---
+++ src/nuevoproyecto2b/NuevoProyecto2B.java
@@ -0,0 +1,28 @@
+/*
+ * Lenguajes de programación I
+ * Facultad de Ingeniería
+ * Universidad Autónoma de Campeche
+ */
+package nuevoproyecto2b;
+
+/**
+ *
+ * @author danieljaimes
+ */
+public class NuevoProyecto2B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int edad = 23;
+        System.out.println("edad = " + edad);
+        
+        // Primer commit
+        
+        String grupo = "2B";
+        System.out.println("grupo = " + grupo);
+    }
+    
+}
